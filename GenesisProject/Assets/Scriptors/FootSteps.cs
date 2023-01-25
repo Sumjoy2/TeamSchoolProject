33c7aca1c5ac3b18995425b96f1da3a5f9808d6d
CMDJ2004 <christophermahasongkham99@gmail.com>
2023-01-25 11:57:08 -0800

    DJ

---
+++ GenesisProject/Assets/Scriptors/FootSteps.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    public AudioSource footstepsSound;
+    float horizontal;
+    float vertical;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if ( horizontal !=0 || vertical != 0)
+        {
+            footstepsSound.enabled = true;
+            Debug.Log("Sonic\n");
+        }
+        else
+        {
+            footstepsSound.enabled = false;
+        }
+    }
+
+
+}
